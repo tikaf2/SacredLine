9579f0f04281cbfc14ced5d11967dd5128567e86
Mentari Tika Fortherina br T <mentaritikaft@gmail.com>
2026-09-15 16:32:23 +0700

    Initial Commit

---
+++ SacredLine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SacredLine
+//
+//  Created by Mentari Tika on 15/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SacredLine/SacredLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SacredLineApp.swift
+//  SacredLine
+//
+//  Created by Mentari Tika on 15/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SacredLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
